0d69da308cb2a75d93d08a787a8511d27f0aa9e7
Ian Ferreira <ian_ferreira93@hotmail.com>
2022-12-14 19:02:36 -0300

    requisito 1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(express.json());
@@ -6,9 +8,19 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-// não remova esse endpoint, e para o avaliador funcionar
+const readFile = async () => {
+  const data = await fs.readFile(path.resolve(__dirname, './talker.json'));
+  return JSON.parse(data);
+};
+
+// não remova esse endpoint, e para o avaliador funcionar!
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_request, response) => {
+  const data = await readFile();
+  return response.status(HTTP_OK_STATUS).send(data);
 });
 
 app.listen(PORT, () => {
